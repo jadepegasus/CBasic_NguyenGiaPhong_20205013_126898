52c64875d2fe5b31ab7e5bb81b7abf7dfddea630
jadepegasus <76956869+jadepegasus@users.noreply.github.com>
2022-01-02 08:24:43 +0700

    Update week1.3.cpp

--- week1.3.cpp
+++ week1.3.cpp
@@ -3,8 +3,9 @@
 •Write a program that accepts two arrays of integers from the user and checks for equality
 */
 #include<stdio.h>
+#define MAX 10000
 int Sosanh(int a[],int b[],int n,int m){
-	if(n!= m) return 0;
+	if(n!= m) return 0; 
 	int j;
 	for(j=0;j<n;j++){
 		if(a[j]!=b[j]) return 0;
@@ -13,10 +14,11 @@ int Sosanh(int a[],int b[],int n,int m){
 }
 int main(){
 	int n,m,i;
-	int a[n],b[m];
 	printf("Nhap so phan tu mang 1:");
 	scanf("%d",&n);
+    int a[MAX],b[MAX];
 	printf("Nhap %d phan tu cua mang 1:",n);
+
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
